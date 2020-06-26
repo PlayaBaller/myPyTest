580070b9e0466bfcb2a7323d7ecd99307473d3dd
pabllr <43369009+pabllr@users.noreply.github.com>
2020-06-26 18:52:51 -0400

    Add files via upload

--- pytestpack/conftest.py
+++ pytestpack/conftest.py
@@ -3,13 +3,34 @@ import pytest
 
 @pytest.yield_fixture()
 def setUp():
-    print("Conftest demo1 method setUp")
+    print("Conftest method setUp")
     yield
-    print("Conftest demo1 method tearDown")
+    print("Conftest method tearDown")
 
 
 @pytest.yield_fixture(scope="module")
-def oneTimeSetUp():
-    print("Conftest demo1 one time method setUp")
+def oneTimeSetUp(browser, osType):
+    print("Conftest one time method setUp")
+    if browser == 'firefox':
+        print("Tests are running on FireFox browser")
+    elif browser == 'chrome':
+        print("Tests are running on Chrome browser")
+    else:
+        print("Please, define a browser which tests would run")
     yield
-    print("Conftest demo1 one time method tearDown")
+    print("Conftest one time method tearDown")
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+    parser.addoption("--osType", help="Type of OS")
+
+
+@pytest.fixture(scope="session")
+def browser(request):
+    return request.config.getoption("browser")
+
+
+@pytest.fixture(scope="session")
+def osType(request):
+    return request.config.getoption("--osType")
